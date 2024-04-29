4b93b621ef19190739e17f7e69648738ef102982
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-04-29 14:20:16 +0200

    finish

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
-const kmuser = prompt("Quanti chilometri vuole percorrere?");
+const kmuser = Number(prompt("Quanti chilometri vuole percorrere?"));
 
-const yearuser = prompt("Quanti anni hai?");
+const yearuser = Number(prompt("Quanti anni hai?"));
 
-const kmprice = ("21")
+const  kmprice = 0.21;
+
+const pricekmuser = kmprice * kmuser ;
+let price = pricekmuser;
+const underage = 18;
+const over65 = 65;
+
+const saleunderage = 20;
+const saleover65 = 40;
+
+if (underage>yearuser) {
+   price= price - (price/100*saleunderage)
+   
+}else if(over65<yearuser) {
+    price=price- (price/100*saleover65)
+}
 
 
 
 
 
+let  currentText;
+currentText = `
+km da percorrere: ${kmuser}  
+anni dell'utente: ${yearuser} 
+prezzo al km ${kmprice}€     
+prezzo dei km da percorrere ${pricekmuser}€
+prezzo finale ${price}€
+`;
 
-
-
-let currentText;
-currentText = `${kmuser} ${yearuser} ${kmprice}`;
 
 
 console.log(currentText);
